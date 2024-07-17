f20d84bb2b39fc65bdb875e462c3bd8b9455daf1
mikedaleo <mikedaleo@outlook.com>
2024-07-17 18:22:12 -0400

    set up controllers and routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,27 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController');
+
 // /users
+router.route('/').get(getUsers).post(createUser);
 
 // bonus /users/:userId/friends/:friendId
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser)
+    .put(addFriend);
+
+router
+    .route('/:userId/friends/:friendId')
+    .put(deleteFriend);
+
+module.exports = router;
